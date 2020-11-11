97f9b5ec3874764352ee6bd31d6ec1bcfa71ea30
Cube <ten.kozlowski@gmail.com>
2020-11-11 14:09:20 +0100

    dummy test2

--- ssg.py
+++ ssg.py
@@ -4,10 +4,9 @@ from ssg.site import Site
 
 def main(source='content', dest='dist'):
     config = {'source':source, 'dest':dest}
-    site = Site(**config)
-    site.build()
+    Site(**config).build()
 
-
+typer.run(main)
 
 
 
--- ssg/site.py
+++ ssg/site.py
@@ -9,15 +9,15 @@ class Site:
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory = self.dest + "/" + path.relative_to()
-        mkdir(directory,parents=True,exists_ok=True)
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True,exists_ok=True)
 
     def build(self):
-        mkdir(self.dest, parents=True, exists_ok=True)
+        self.dest(parents=True, exists_ok=True)
 
         for path in self.source.rglob("*"):
             if path.is_dir():
-                mkdir(path)
+                self.create_dir(path)
 
 
 
