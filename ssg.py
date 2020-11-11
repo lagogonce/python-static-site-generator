65f264bc539ffb56be390e696333f0a5ea163a5c
Cube <ten.kozlowski@gmail.com>
2020-11-11 15:07:24 +0100

    dummy test9

--- ssg.py
+++ ssg.py
@@ -2,8 +2,8 @@ import typer
 from os import mkdir
 from ssg.site import Site
 
-def main(source='content', dest='dist'):
-    config = {'source':source, 'dest':dest}
+def main(source="content", dest="dist"):
+    config = {"source":source, "dest":dest}
     Site(**config).build()
 
 typer.run(main)
